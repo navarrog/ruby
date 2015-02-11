35f230fc188110e2f1ac4ff89690944750298b12
Gustavo <gustavo_nav@hotmail.com>
2015-02-11 07:42:10 -0200

    Cadastro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Devise gem - Needs to change localhost to actual application
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
 end
